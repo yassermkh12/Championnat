0a4a8e1dff8bb42f6c7069277bd30b47c12273dd
yassermkh12 <115877348+yassermkh12@users.noreply.github.com>
2023-12-10 15:50:43 +0100

    15:50 championnat crud

--- championnat/src/main/java/com/example/championnat/services/IChampionnatService.java
+++ championnat/src/main/java/com/example/championnat/services/IChampionnatService.java
@@ -1,4 +1,14 @@
 package com.example.championnat.services;
 
+import com.example.championnat.entitiesDto.ChampionnatDto;
+
+import java.util.List;
+
 public interface IChampionnatService {
+    public List<ChampionnatDto> getAllChampionnat();
+    public ChampionnatDto getChampionnatById(Long id);
+    public ChampionnatDto saveChampionnat(ChampionnatDto championnatDto);
+    public List<ChampionnatDto> saveListChampionnat(List<ChampionnatDto> championnatDtos);
+    public ChampionnatDto updateChampionnat(Long id,ChampionnatDto championnatDto);
+    public void deleteChampionnat(Long id);
 }
--- championnat/src/main/java/com/example/championnat/services/impl/ChampionnatService.java
+++ championnat/src/main/java/com/example/championnat/services/impl/ChampionnatService.java
@@ -1,4 +1,53 @@
 package com.example.championnat.services.impl;
 
-public class ChampionnatService {
+import com.example.championnat.entities.Championnat;
+import com.example.championnat.entitiesDto.ChampionnatDto;
+import com.example.championnat.repositories.IChampionnatRepository;
+import com.example.championnat.services.IChampionnatService;
+import com.example.championnat.transformers.ChampionnatTransformer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class ChampionnatService implements IChampionnatService {
+    @Autowired
+    private IChampionnatRepository championnatRepository;
+
+    public List<ChampionnatDto> getAllChampionnat(){
+        List<Championnat> championnats = championnatRepository.findAll();
+        return ChampionnatTransformer.entityToDtoList(championnats);
+    }
+    public ChampionnatDto getChampionnatById(Long id){
+        Championnat championnat = championnatRepository.findById(id).orElse(null);
+        return ChampionnatTransformer.entityToDto(championnat);
+    }
+    public ChampionnatDto saveChampionnat(ChampionnatDto championnatDto){
+        Championnat championnat = ChampionnatTransformer.dtoToEntity(championnatDto);
+        championnatRepository.save(championnat);
+        return ChampionnatTransformer.entityToDto(championnat);
+    }
+    public List<ChampionnatDto> saveListChampionnat(List<ChampionnatDto> championnatDtos){
+        List<Championnat> championnats = ChampionnatTransformer.dtoToEntityList(championnatDtos);
+        championnatRepository.saveAll(championnats);
+        return ChampionnatTransformer.entityToDtoList(championnats);
+    }
+    public ChampionnatDto updateChampionnat(Long id,ChampionnatDto championnatDto){
+        Championnat championnat = championnatRepository.findById(id).orElse(null);
+        if(championnat != null){
+            championnat.setTypeChampionnat(championnatDto.getTypeChampionnat());
+            championnat.setNomDuChampionnat(championnatDto.getNomDuChampionnat());
+            championnat.setDateDebutChampionnat(championnatDto.getDateDebutChampionnat());
+            championnat.setDateFinChampionnat(championnatDto.getDateFinChampionnat());
+            championnat.setSaison(championnatDto.getSaison());
+
+            return ChampionnatTransformer.entityToDto(championnat);
+        }else {
+            return null;
+        }
+    }
+    public void deleteChampionnat(Long id){
+        championnatRepository.deleteById(id);
+    }
 }
