71611fa18446eac2fc316a1daa5c2ac80c0044f2
yassermkh12 <115877348+yassermkh12@users.noreply.github.com>
2023-12-12 15:07:49 +0100

    15:07 championnat crud

--- championnat/src/main/java/com/example/championnat/services/impl/ChampionnatService.java
+++ championnat/src/main/java/com/example/championnat/services/impl/ChampionnatService.java
@@ -6,11 +6,12 @@ import com.example.championnat.exceptions.NotFoundException;
 import com.example.championnat.repositories.IChampionnatRepository;
 import com.example.championnat.services.IChampionnatService;
 import com.example.championnat.transformers.ChampionnatTransformer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
-
+@Slf4j
 @Service
 public class ChampionnatService implements IChampionnatService {
     @Autowired
@@ -18,41 +19,50 @@ public class ChampionnatService implements IChampionnatService {
 
     public List<ChampionnatDto> getAllChampionnat(){
         List<Championnat> championnats = championnatRepository.findAll();
+        log.info("les championnats dans la base de donnees sont : "+ championnats);
         return ChampionnatTransformer.entityToDtoList(championnats);
     }
     public ChampionnatDto getChampionnatById(Long id) throws NotFoundException {
         Championnat championnat = championnatRepository.findById(id).orElse(null);
         if(championnat != null) {
+            log.info("le championnat avec l id : "+ id + " existe " + championnat);
             return ChampionnatTransformer.entityToDto(championnat);
         }else {
+            log.info("il n y a pas de championnat avec id "+ id);
             throw new NotFoundException("Il n y a pas de championnat avec l id : "+ id);
         }
     }
     public ChampionnatDto saveChampionnat(ChampionnatDto championnatDto){
         Championnat championnat = ChampionnatTransformer.dtoToEntity(championnatDto);
         championnatRepository.save(championnat);
+        log.info("le championnat est enregistres avec id : "+ championnat.getId());
         return ChampionnatTransformer.entityToDto(championnat);
     }
     public List<ChampionnatDto> saveListChampionnat(List<ChampionnatDto> championnatDtos){
         List<Championnat> championnats = ChampionnatTransformer.dtoToEntityList(championnatDtos);
         championnatRepository.saveAll(championnats);
+        log.info("les championnats sont enregistres");
         return ChampionnatTransformer.entityToDtoList(championnats);
     }
     public ChampionnatDto updateChampionnat(Long id,ChampionnatDto championnatDto) throws NotFoundException {
         Championnat championnat = championnatRepository.findById(id).orElse(null);
         if(championnat != null){
+            log.info("le championnat avec l id : "+ id + " existe " + championnat);
             championnat.setTypeChampionnat(championnatDto.getTypeChampionnat());
             championnat.setNomDuChampionnat(championnatDto.getNomDuChampionnat());
             championnat.setDateDebutChampionnat(championnatDto.getDateDebutChampionnat());
             championnat.setDateFinChampionnat(championnatDto.getDateFinChampionnat());
             championnat.setSaison(championnatDto.getSaison());
 
+            log.info("le championnat est mis a jour "+ championnat);
             return ChampionnatTransformer.entityToDto(championnat);
         }else {
+            log.info("il n y a pas de championnat avec id "+ id);
             throw new NotFoundException("Il n y a pas de championnat avec l id : "+ id);
         }
     }
     public void deleteChampionnat(Long id){
         championnatRepository.deleteById(id);
+        log.info("le championnat avec id : "+ id + " est supprime");
     }
 }
