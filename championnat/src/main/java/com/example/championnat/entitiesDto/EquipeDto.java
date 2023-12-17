76890d4b7033eabe3eedda88590921d7d02ddbee
yassermkh12 <115877348+yassermkh12@users.noreply.github.com>
2023-12-17 14:05:18 +0100

    14:05 equipe crud

--- championnat/src/main/java/com/example/championnat/entitiesDto/EquipeDto.java
+++ championnat/src/main/java/com/example/championnat/entitiesDto/EquipeDto.java
@@ -1,6 +1,10 @@
 package com.example.championnat.entitiesDto;
 
 import com.example.championnat.entities.enums.StatusEquipe;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +14,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class EquipeDto {
     private Long id;
+    @NotBlank(message = "le champ ne doit pas etre blank")
+    @NotNull(message = "le champ ne doit pas etre null")
+    @NotEmpty(message = "le champ ne doit pas etre vide")
     private String nomEquipe;
+    @NotBlank(message = "le champ ne doit pas etre blank")
+    @NotNull(message = "le champ ne doit pas etre null")
+    @NotEmpty(message = "le champ ne doit pas etre vide")
     private String paysEquipe;
+    @NotBlank(message = "le champ ne doit pas etre blank")
+    @NotNull(message = "le champ ne doit pas etre null")
+    @NotEmpty(message = "le champ ne doit pas etre vide")
+    @Pattern(regexp = "^\\d{4}$", message = "la saison doit etre de la facon suivant num exemple : (2024)")
     private String anneFondation;
     //image logo de l equipe
     //relation entrainneur
@@ -20,6 +34,9 @@ public class EquipeDto {
     //relation sponsor
     //relation palmares
     //relation efictif actuel
-    private String budgetEquipe;
+//    @NotBlank(message = "le champ ne doit pas etre blank")
+//    @NotNull(message = "le champ ne doit pas etre null")
+//    @NotEmpty(message = "le champ ne doit pas etre vide")
+    private Long budgetEquipe;
     private StatusEquipe statusEquipe;
 }
